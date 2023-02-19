db21389d4a47fd26b275f5e8f5d8e667f9b6e639
gemrey <gemreyranola@gmail.com>
2023-02-19 19:29:24 +0800

    Update 1.1

--- process/utils.php
+++ process/utils.php
@@ -1,6 +1,24 @@
 <?php
 	$pdo = require '../connection.php';
 
+	if (isset($_POST['addUserBtn'])) {
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$username = $_POST['username'];
+		$upass = $_POST['upass'];
+
+		if (checkUsername($pdo, $username) == True) {
+			echo '<script>alert("Username Already Exist!!")</script>';
+		} else {
+			addUser($pdo, $username, $firstname, $lastname, $upass);
+			echo '<script>alert("Added Succesfully")</script>';
+		};
+		unset($_POST['addUserBtn']);
+	};
+
+
+
+
 	function addUser($pdo, $username, $firstname, $lastname, $upass) {
 		try {
 			$sql = 'INSERT INTO User_List (username, firstname, lastname, upass) VALUES (:username, :firstname, :lastname, :upass)';
@@ -17,5 +35,22 @@
         $sql = null;
     };
 
-	//addUser($pdo, 'naruto123', 'ruegie', 'ricohermoso', 'cutiepogi123');
+
+    function checkUsername($pdo, $username) {
+    	try {
+    		$sql = 'SELECT count(username) as rowCount FROM User_List where username=:username';
+    		$statement = $pdo->prepare($sql);
+    		$statement->bindValue(':username', $username);
+    		$statement->execute();
+    		$row = $statement->fetch(PDO::FETCH_ASSOC);
+    		if ($row['rowCount'] > 0) {
+    			return True;
+    		} else {
+    			return False;
+    		}
+    	} catch (Exception $e) {
+    		echo 'Caught Exception: ', $e->getMessage(), '\n';
+    	}
+    };
+	
  ?>
