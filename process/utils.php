43519bdfbcc9e2cf7bf767fd2d66f3d8ba3ca377
gemrey <gemreyranola@gmail.com>
2023-02-20 10:35:21 +0800

    Ongoing Uploadin files

--- process/utils.php
+++ process/utils.php
@@ -14,9 +14,33 @@
 			echo '<script>alert("Added Succesfully")</script>';
 		};
 		unset($_POST['addUserBtn']);
+	} elseif (isset($_POST['addBookBtn'])) {
+		$title = $_POST['title'];
+		$author = $_POST['author'];
+		$description = $_POST['description'];
+		$categoryID = $_POST['options'];
+
+		if (JVB) {
+			// code...
+		}
+		// $target_dir = '../media/';
+		// $target_file = $target_dir.basename($_FILES['image']['name']);
+		
+		// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		// $check = getimagesize($_FILES["image"]["tmp_name"]);
+
+		// if($check !== false) {
+		//     addBook($pdo, $title, $author, $description, $target_file, $categoryID);
+ 		// 	move_uploaded_file($_FILES['image']['tmp_name'],$target_file);
+ 		// 	if (move_uploaded_file($_FILES['image']['tmp_name'],$target_file)) {
+ 		// 		echo '<script>alert("Upload success")</script>';
+ 		// 	}
+		// 	echo '<script>alert("Book Publish Successfully")</script>';
+		//   } else {
+		//     echo '<script>alert("Error Publishing Book!!")</script>';
+		//   };
+		
 	};
-
-
 
 
 	function addUser($pdo, $username, $firstname, $lastname, $upass) {
@@ -35,16 +59,18 @@
         $sql = null;
     };
 
+   
 
 
-    function addBook($pdo, $title, $author, $description, $image) {
+    function addBook($pdo, $title, $author, $description, $image, $categoryID) {
     	try {
-    		$sql = 'INSERT INTO Book_List (title, author, description, image) VALUES (:title, :author, :description, :image)';
+    		$sql = 'INSERT INTO Book_List (title, author, description, image, categoryID) VALUES (:title, :author, :description, :image, :categoryID)';
     		$statement = $pdo->prepare($sql);
     		$statement->bindValue(':title', $title);
     		$statement->bindValue(':author', $author);
     		$statement->bindValue(':description', $description);
     		$statement->bindValue(':image', $image);
+    		$statement->bindValue(':categoryID', $categoryID);
     		$statement->execute();
     	}catch(Exception $e) {
     		echo 'Message: '.$e->getMessage();
@@ -52,6 +78,30 @@
     	$pdo = null;
     	$sql = null;
     };
+
+
+
+
+
+
+    function showCategories($pdo) {
+    	$sql = 'SELECT * from Categories';
+    	$statement = $pdo->query($sql);
+    	$row = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    	echo '<select name="options">';
+    	foreach ($row as $r) {
+    		echo '<option value="'.$r['categoryID'].'">'
+    		.$r['name'] .'
+    		</option>';
+    	}
+    	echo '</select>';
+
+        $pdo = null;
+        $sql = null;
+    };
+
+
 
 
 
---
+++ template/addBook.php
@@ -0,0 +1,53 @@
+<?php
+  include '../process/utils.php';
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Book Renter</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="../static/css/index.css" rel="stylesheet">
+</head>
+<body>
+  <div style="width:50%;margin: auto;">
+<form action="#" method="POST" enctype="multipart/form-data">
+
+  <!-- Title input -->
+  <div class="form-outline mb-4">
+    <input type="text" id="titl" class="form-control" name="title" />
+    <label class="form-label" for="titl">Title</label>
+  </div>
+
+  <!-- Author input -->
+  <div class="form-outline mb-4">
+    <input type="text" id="auth" class="form-control" name="author" />
+    <label class="form-label" for="auth">Author</label>
+  </div>
+
+  <!-- Description input -->
+  <div class="form-outline mb-4">
+    <textarea class="form-control" id="desc" rows="4" name="description"></textarea>
+    <label class="form-label" for="desc">Description</label>
+  </div>
+
+  <!-- Image input -->
+  <div class="form-outline mb-4">
+    <input type="file" name="image" id="image"><br>
+    <label class="form-label" for="image">Select a Image</label>
+  </div>
+
+  <div class="form-outline mb-4">
+    <?php
+    showCategories($pdo);
+    ?>
+  </div>
+
+  <!-- Submit button -->
+  <button type="submit" class="btn btn-primary btn-block mb-4" name="addBookBtn">Add a book</button>
+</form>
+</div>
+</body>
